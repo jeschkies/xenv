6dcb87052291aa50012b1d387679af9155fcff02
Karsten Jeschkies <k@jeschkies.xyz>
2019-07-01 11:27:48 +0200

    Read .xenv.toml and expose hard coded path.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use std::fs;
+use toml;
+
+#[derive(Deserialize)]
+struct XEnv {
+    packages: Packages,
+}
+
+#[derive(Deserialize)]
+struct Packages {
+    mesos: String, // TODO: make this pair generic.
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let content = fs::read(".xenv.toml")?;
+    let t: XEnv = toml::de::from_slice(&content)?;
+    let export_path = format!("/Users/kjeschkies/.mesos/mesos_versions/{}/bin:/Users/kjeschkies/.mesos/mesos_versions/{}/sbin",
+                              t.packages.mesos, t.packages.mesos);
+    println!("{}", export_path);
+    Ok(())
 }
